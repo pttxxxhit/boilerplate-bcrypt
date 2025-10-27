3a6c86235b43b732bf38a3bdf0e0665078ae467c
pttxxxhit <ernestomerinocc@gmail.com>
2025-10-26 21:46:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -50,3 +50,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
 });
+const listener = app.listen(process.env.PORT || 5000, () => {
+  console.log('Listening on port: ' + listener.address().port);
+});
+
